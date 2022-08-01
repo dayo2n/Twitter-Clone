8bb34994823175a20929f34c7ca4e7ab2affe3a5
dayo2n <0217dayun@naver.com>
2022-08-01 14:50:56 +0900

    initial setting

---
+++ TwitterClone/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  TwitterClone
+//
+//  Created by 문다 on 2022/08/01.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
