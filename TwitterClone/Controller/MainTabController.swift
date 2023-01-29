06ac97e69f9142793d19e09797bfb6f75441bec8
dayo2n <0217dayun@naver.com>
2023-01-29 19:35:31 +0900

    [feat] button에 custom color 지정, action handler 지정

--- TwitterClone/Controller/MainTabController.swift
+++ TwitterClone/Controller/MainTabController.swift
@@ -13,8 +13,9 @@ class MainTabController: UITabBarController {
     let actionButton: UIButton = {
         let button = UIButton(type: .system)
         button.tintColor = .white
-        button.backgroundColor = .blue
+        button.backgroundColor = .twitterBlue
         button.setImage(UIImage(systemName: "plus"), for: .normal)
+        button.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
         return button
     }()
     
@@ -25,6 +26,11 @@ class MainTabController: UITabBarController {
 
         configureViewContoller()
         configureUI()
+    }
+    
+    // MARK: - Selectors
+    @objc func actionButtonTapped() {
+        print("button tapped")
     }
     
     // MARK: - Helpers
--- TwitterClone/Utils/Extensions.swift
+++ TwitterClone/Utils/Extensions.swift
@@ -90,7 +90,5 @@ extension UIColor {
         return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
     }
     
-    static let mainBlue = UIColor.rgb(red: 29, green: 161, blue: 242)
-    static let customRed = UIColor.rgb(red: 220, green: 78, blue: 65)
-    static let customPurple = UIColor.rgb(red: 128, green: 87, blue: 194)
+    static let twitterBlue = UIColor.rgb(red: 29, green: 161, blue: 242)
 }
